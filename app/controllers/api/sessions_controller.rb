1e2675d907db1e36a4faaaaa26b0205f93cd4c4e
Raphael Quintanilha dos Santos <raphael.quintanilha@wpensar.com.br>
2016-06-04 19:05:34 -0300

    remove binding.pry on api

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,6 @@
 class Api::SessionsController < ApplicationController
 
     def create
-        binding.pry
         user = User.find_by(email: session_params[:email])
         if user
             if (session_params[:social_login])
