c79648ef42fe04c58d0ac5e99df4be5f23340ab9
Hugo Arenari <hugoarenari@gmail.com>
2016-06-11 17:44:00 -0300

    correções email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,5 +2,6 @@ class ApplicationMailer < ActionMailer::Base
   
   default from: "\"Triskele\" <meajuda@triskele.me>"
   layout 'mailer'
+  @base_url = "http://development.triskele.me:3000/"
   
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,5 +1,6 @@
 # config/initializers/time_formats.rb
 Time::DATE_FORMATS[:day_and_hour] = "%d/%m/%Y - %H:%M"
+Time::DATE_FORMATS[:day_at_hour] = "%d/%m/%Y às %H:%M"
 Time::DATE_FORMATS[:day] = "%d/%m/%Y"
 Time::DATE_FORMATS[:simple_day] = "%d/%m"
 Time::DATE_FORMATS[:hour] = "%H:%M"
