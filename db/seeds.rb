98793713228e22b1a7b74cbd886442f82873e1aa
Raphael Quintanilha dos Santos <raphael.quintanilha@wpensar.com.br>
2016-07-27 23:32:51 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,27 @@
+require 'net/http'
+require 'net/https' # for ruby 1.8.7
+require 'json'
 
+module BRPopulate
+  def self.states
+    http = Net::HTTP.new('raw.githubusercontent.com', 443); http.use_ssl = true
+    JSON.parse http.get('/celsodantas/br_populate/master/states.json').body
+  end
 
-country_list = [
-  [ "Brasil", "BR" ]
-]
-country_list.each do |name, acronym|
-  Country.create( name: name, acronym: acronym )
+  def self.populate
+   country_obj = Country.new(:name => "Brasil", :acronym => "BR", active => true)
+   country_obj.save
+   states.each do |state|
+      state_obj = State.new(:acronym => state["acronym"], :name => state["name"], :active => false)
+      state_obj.save
+      state["cities"].each do |city|
+        c = City.new
+        c.name = city["name"]
+        c.state = state_obj
+        c.save
+      end
+    end
+   end
 end
 
-state_list = [
-  [ "Rio de Janeiro", "RJ" ]
-]
-country_list.each do |name, population|
-  Country.create( name: name, acronym: population )
-end
-
+BRPopulate.populate
